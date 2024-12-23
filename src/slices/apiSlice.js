38308d77e4cf7a899134a98d5402c411918dac94
suhair20 <moidheensuhair@gmail.com>
2024-12-23 17:38:32 +0530

    onechange

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 
-const baseQuery=fetchBaseQuery({baseUrl:''})
+const baseQuery=fetchBaseQuery({baseUrl:'https://user-management-backend-cn9a.onrender.com'})
 export const apiSlice=createApi({
     baseQuery,
     tagTypes:['User'],
