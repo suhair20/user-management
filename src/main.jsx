fc1fa2fd1505784b3256a0a462008e7860453d32
suhair20 <moidheensuhair@gmail.com>
2025-01-04 09:38:59 +0530

    main

--- src/main.jsx
+++ src/main.jsx
@@ -33,10 +33,10 @@ const router =createBrowserRouter(
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <Provider store={store} >
-  <React.StrictMode>
+  
     
    <RouterProvider router={router} />
     
-  </React.StrictMode>
+ 
   </Provider>
 )
